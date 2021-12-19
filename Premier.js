0d345e5b9aa55457340d7e5809e82ff64a2d0909
Benoit GRASSET <32497923+benoitgrasset@users.noreply.github.com>
2021-12-19 11:48:08 +0100

    Create Premier.js

---
+++ Premier.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  const N = 5000;
+
+  const isPrimer = (nbr) => {
+    for (let i = 2; i < nbr; i++) if (nbr % i === 0) return false;
+    return nbr > 1;
+  };
+
+  const getArray = () => {
+    let list = [];
+    for (let i = 0; i <= N; i++) {
+      list.push(i);
+    }
+    return list;
+  };
+
+  const array = getArray();
+
+  return (
+    <div>
+      <h1>Nombres premiers</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {array.map((e) => {
+          const premier = isPrimer(e);
+          const premierStyle = premier && {
+            color: 'red',
+            fontWeight: 700,
+            background: 'yellow',
+          };
+          return (
+            <div
+              style={{
+                padding: '3px',
+                border: '1px solid black',
+                ...premierStyle,
+              }}
+            >
+              {e}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
