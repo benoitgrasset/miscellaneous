48d617358eb371dcef246f11441a91ba0d246794
Benoit GRASSET <32497923+benoitgrasset@users.noreply.github.com>
2025-03-26 12:27:42 +0100

    Update generator.js

--- generator.js
+++ generator.js
@@ -8,3 +8,32 @@ function* fetchUsers() {
 
 const fetchUsersPromise = fetchUsers().next().value;
 fetchUsersPromise.then(res => console.log('response', res));
+
+
+
+const BASE_URL = "https://jsonplaceholder.typicode.com/users";
+const LIMIT = 10;
+
+async function* fetchUsers() {
+  let page = 0;
+  while (true) {
+    yield await fetch(`${BASE_URL}?_limit=${LIMIT}&_start=${LIMIT * page}`)
+      .then((res) => res.json())
+      .catch((err) => {
+        console.error(err);
+      });
+
+    page++;
+  }
+}
+
+async function* fetchUser() {
+  for await (const page of fetchUsers()) {
+    for (const user of page) {
+      yield user;
+    }
+  }
+}
+
+const fetchUsersPromise = fetchUsers().next().value;
+fetchUsersPromise.then(res => console.log(res));
